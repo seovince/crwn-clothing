7caf94f9f5c268955682ea3fe1ac25aad90864a9
Vincentius Caesario Yudhistira <vincentiuscy@gmail.com>
2021-12-12 20:05:38 +0700

    add header component and links to home/shop page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <Routes>
           <Route exact path='/' element={<HomePage />} />
           <Route path='/shop' element={<ShopPage />} />
