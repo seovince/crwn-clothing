779b0ac460eaf5942c8b37ef014f81b0e0b591f4
Vincentius Caesario Yudhistira <vincentiuscy@gmail.com>
2021-12-12 20:52:00 +0700

    added custom button component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 import './App.css';
@@ -20,6 +21,7 @@ class App extends React.Component {
         <Routes>
           <Route exact path='/' element={<HomePage />} />
           <Route path='/shop' element={<ShopPage />} />
+          <Route path='/signin' element={<SignInAndSignUpPage />} />
         </Routes>
       </div>
     );
