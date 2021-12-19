cf96096561ce8a447ae7bfe19de08b438224fe07
Vincentius Caesario Yudhistira <vincentiuscy@gmail.com>
2021-12-19 15:02:58 +0700

    added stripe integration, updated favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg"
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51K8JkwH0nS0vBnbusjzdPfRkBVrvKwHHWmyaA3GYCCusLuwvc8BZimpg4wnRANcadNsqlZoUrqKm1OAE1dvAdmiY00A77l4apc'
+
+    const onToken = token => {
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout 
+            label="Pay Now"
+            name="CRN Clothing Ltd."
+            billingAddress
+            shippingAddress
+            image="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/android.svg"
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
